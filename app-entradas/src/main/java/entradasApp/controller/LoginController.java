9e8fa65fb55a9e56f188ca4db61c3e178e7dba4e
Gustavo Starace <practicodecocina@gmail.com>
2023-06-16 17:58:25 +0200

    refactorizacion OK persistencia data.sql ok, clase pase de oro en funcionamiento. a falta de trabajar la tabla intermedia CORS *

--- app-entradas/src/main/java/entradasApp/controller/LoginController.java
+++ app-entradas/src/main/java/entradasApp/controller/LoginController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/login")
-@CrossOrigin(origins = "*")
 public class LoginController {
 
     private final LoginService loginService;
