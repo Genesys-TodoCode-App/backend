36d9c093b3668f6e8b74c47747bad3c68c129819
Gustavo Starace <practicodecocina@gmail.com>
2023-06-16 18:31:57 +0200

    Revert "refactorizacion OK persistencia data.sql ok, clase pase de oro en funcionamiento. a falta de trabajar la tabla intermedia CORS *"
    This reverts commit 9e8fa65fb55a9e56f188ca4db61c3e178e7dba4e.

--- app-entradas/src/main/java/entradasApp/controller/LoginController.java
+++ app-entradas/src/main/java/entradasApp/controller/LoginController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/login")
+@CrossOrigin(origins = "*")
 public class LoginController {
 
     private final LoginService loginService;
